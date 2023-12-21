896dda5227d1718eefcdddda9f249de863d0ebe3
amos zohar <azamoszohar@gmail.com>
2023-12-21 18:25:02 +0200

    finished algorithm. working well. main and parser are the files

--- main.py
+++ main.py
@@ -43,16 +43,96 @@ class Div(BinExp):
     def __init__(self, left, right) -> None:
         super().__init__(left, right)
     def calc(self):
+        if self.right.calc() == 0:
+            raise ValueError("Division by zero")
         return self.left.calc() / self.right.calc()
 
-def main():
-    a = Num(345)
-    b = Num(5)
-    c = Num(1296)
-    d = Num(216)
-    #express = "345/5-(1296/216)"
-    calculation1 = Minus(Div(a,b),Div(c,d))
-    res = 345/5-(1296/216)
-    print(calculation1.calc()==res)
-main()
+#implement the parser function here
+def parser(expression)->double:
+    Q = []#a queue, only difference to a stack is to dequeue, must use Q.pop(0)
+    S = []#a stack
+    i=0
+    #NOTE to self: there are multiple iterations occouring:
+    #               1.The external one,for the input expression, going over characters one by one
+    #               1.1. if we detect a digit, there is another, parallel check over the expression, to see
+    #                    where the number ends(multiple digits number)
+    #               2.if the char is - or +, we need to pop all * and / in the stack into the queue
+    #                 until we reach the first -,+ or (.
+    #               3.if the char is ), we need to pop all the ops in the stack, until we find a (.
+    while i<len(expression):
+        if expression[i].isnumeric():   
+            lsd = extract_whole_number(expression,i)
+            num_string = expression[i:lsd]
 
+            parsed_num = float(num_string)
+            Q.append(Num(parsed_num))
+            i=lsd
+        else:
+            char = expression[i]
+            if char == "(" or char == "/" or char =="*":
+                S.append(char)
+            elif char == "-" or char == "+":
+                #first, a check to differentiate between the operator Minus (a-b) vs the sign (-5)
+                if char =="-" and S[-1]=="(" and i+1<len(expression) and expression[i+1].isnumeric():
+                    least_significant_bit = extract_whole_number(expression,i+1)
+                    neg_num_string = expression[i:least_significant_bit]
+                    parsed_neg_num = float(neg_num_string)
+                    Q.append(Num(parsed_neg_num))
+                    i=least_significant_bit
+                    continue
+                else:
+                    #NEED TO CONSERVE ORDER OF OPERATIONS: All DIV AND MUL OPS MUST COME BEFORE PLUS AND MINUS OPS
+                    while S and (S[-1]=="*" or S[-1]=="/"):
+                        Q.append(S.pop())
+                    S.append(char)
+            elif char == ")":#need to pop all stack items into the queue,until we reach an (
+                while S:
+                    stack_top = S.pop()
+                    if stack_top == "(":
+                        break
+                    else:
+                        Q.append(stack_top)
+            i+=1
+
+    #Now, to dump what operations may remain in the Stack into the Queue
+
+    while S:#NOTE: this could be the source of the problem,
+            #since here we just dump whatever leftover operators are in the stack
+        Q.append(S.pop())
+
+    #--------FINISHED going over input expression and preparing the output queue--------------
+
+    #Now, I must dequeue all the numbers into the stack until meeting an op, perform that op on the stack head
+    #and its predecessor, pop `em, and append the result of the op into the stack, ad infinitum
+    while Q:
+        element = Q.pop(0)
+        if isinstance(element,Num):
+            S.append(element)
+        else:
+            op1 = S.pop()
+            op2 = S.pop()
+            res = 0
+            if element == "/":
+                res = Div(op1,op2)
+            elif element == "*":
+                res = Mul(op1,op2)
+            elif element == "-":
+                res = Minus(op1,op2)
+            elif element == "+":
+                res = Plus(op1,op2)
+            S.append(Num(res.calc()))
+
+    return S.pop().calc()
+
+def extract_whole_number(expression,start_index):
+    i = start_index
+    while i<len(expression) and expression[i].isnumeric() or expression[i]==".":#needed i<len(expression), otherwise error when the last
+                                                          #characters form a number
+        i+=1
+
+    return i
+
+expression = "(27-(-66)+39)"
+res = parser(expression)
+exp = eval(expression)
+print(f"res={res},exp={exp}, they are { 'not' if res!=exp else ''} equal")
--- parser.py
+++ parser.py
@@ -8,14 +8,14 @@ class Expression(ABC):
         pass
 
 # implement the classes here
-class Num(ABC):
+class Num(Expression):
     def __init__(self,x) -> None:
         super().__init__()
-        self.x = x
+        self.x=x
     def calc(self)->double:
         return self.x
-    
-class BinExp(ABC):
+
+class BinExp(Expression):
     def __init__(self,left,right) -> None:
         super().__init__()
         self.left = left
@@ -24,105 +24,143 @@ class BinExp(ABC):
 class Plus(BinExp):
     def __init__(self, left, right) -> None:
         super().__init__(left, right)
-    def calc(self):
+    
+    def calc(self) -> double:
         return self.left.calc() + self.right.calc()
 
 class Minus(BinExp):
     def __init__(self, left, right) -> None:
         super().__init__(left, right)
-    def calc(self):
+    
+    def calc(self) -> double:
         return self.left.calc() - self.right.calc()
-
+    
 class Mul(BinExp):
     def __init__(self, left, right) -> None:
         super().__init__(left, right)
-    def calc(self):
-        return self.left.calc() * self.right.calc()
     
+    def calc(self) -> double:
+        return self.left.calc() * self.right.calc()
+
 class Div(BinExp):
     def __init__(self, left, right) -> None:
         super().__init__(left, right)
-    def calc(self):
-        return self.left.calc() / self.right.calc()
+    
+    def calc(self) -> double:
+        y = self.right.calc()
+        if y==0:
+            return None
+        return self.left.calc() / y
+
 
 #implement the parser function here
 def parser(expression)->double:
-    Q = []#a queue, only difference to a stack is to dequeue, must use Q.pop(0)
-    S = []#a stack
-    i=0
-    #NOTE to self: there are multiple iterations occouring:
-    #               1.The external one,for the input expression, going over characters one by one
-    #               1.1. if we detect a digit, there is another, parallel check over the expression, to see
-    #                    where the number ends(multiple digits number)
-    #               2.if the char is - or +, we need to pop all * and / in the stack into the queue
-    #                 until we reach the first -,+ or (.
-    #               3.if the char is ), we need to pop all the ops in the stack, until we find a (.
-    while i<len(expression):
-        if expression[i].isnumeric():   
-            lsd = extract_whole_number(expression,i)
-            num_string = expression[i:lsd]
+    queue = []
+    stack = []
+    i = 0
+    n = len(expression)
+    while i<n:
+        c = expression[i]
+        #first, I check if it is the begining of a substring representing a
+        #negative number.
+        #Negative numbers with absolute value x are represented as '(-x)'
+        if c == "(" and expression[i+1] == '-':
+            #need to extract the entire number, and push it to the end of the queue
+            j = number_end(expression,i+2)
+            queue.append(Num(double(expression[i+1:j])))
+            i = j+1#since it is '(-x)', after the end of the number must come a ')'
+            continue#extracted the negative number, gotta go extract the next token
+        
+        #if encountered a positive number, just extract it and append to the queue
+        if c.isnumeric():
+            j = number_end(expression,i)
+            numstr = expression[i:j]
+            #print(numstr)
+            queue.append(Num(double(numstr)))
+            i=j
+            continue
 
-            parsed_num = float(num_string)
-            Q.append(Num(parsed_num))
-            i=lsd
-        else:
-            char = expression[i]
-            if char == "(" or char == "/" or char =="*":
-                S.append(char)
-            elif char == "-" or char == "+":
-                #NEED TO CONSERVE ORDER OF OPERATIONS: All DIV AND MUL OPS MUST COME BEFORE PLUS AND MINUS OPS
-                while S:
-                    stack_top = S.pop()
-                    if stack_top == "/" or stack_top == "*":
-                        Q.append(stack_top)
-                    else:
-                        S.append(stack_top)#I forgot to return the stack top, causing issues
-                        break
-                S.append(char)
-            elif char == ")":#need to pop all stack items into the queue,until we reach an (
-                while S:
-                    stack_top = S.pop()
-                    if stack_top == "(":
-                        break
-                    else:
-                        Q.append(stack_top)
+        #not explicitly stated, but if we got here,
+        #the ramining options are either an operator or a parenthesis.
+        #Thus, must seperate to 2 cases: if parenthesis, or an operator:
+        if c=="(":
+            stack.append(c)
+            i+=1
+            continue
+        if c==")":
+            while stack[-1]!="(":
+                queue.append(stack.pop())
+            stack.pop()
+            i+=1
+            continue
+        #if code reached here, it means expression[i] is not '(',')', a
+        #number, or a negative number (-x).
+        #thus, it must be an operator.
+        #need to seperate to 2 cases: if stack is empty,
+        # i just push into it.
+        #otherwise, I must take into account 2 things, whilst
+        #observing the stack head:
+        # 1.order of operations
+        # 2. left associativity of Div and Minus
+        
+        #if stack is empty, just push into it
+        if not stack:
+            stack.append(c)
             i+=1
 
-    #Now, to dump what operations may remain in the Stack into the Queue
-
-    while S:
-        Q.append(S.pop())
-
-    #--------FINISHED going over input expression and preparing the output queue--------------
-
-    #Now, I must dequeue all the numbers into the stack until meeting an op, perform that op on the stack head
-    #and its predecessor, pop `em, and append the result of the op into the stack
-    while Q:
-        element = Q.pop(0)
-        if isinstance(element,Num):
-            S.append(element)
+        #need to also look at what is already in the stack
         else:
-            if len(S)<2:
-                continue
-            op2 = S.pop()
-            op1 = S.pop()
-            res = 0
-            if element == "/":
-                res = Div(op1,op2)
-            elif element == "*":
-                res = Mul(op1,op2)
-            elif element == "-":
-                res = Minus(op1,op2)
-            elif element == "+":
-                res = Plus(op1,op2)
-            S.append(Num(res.calc()))
+            #this check takes care of 2 things: both of the importance of doing
+            #(9-5)+1 instead of 9-(5+1), and the left associativity of -:
+            #10-1-2-3 must be ((10-1)-2)-3
+            if c=="+" or c=="-":
+                if stack[-1] == "-" or stack[-1]=="/" or stack[-1]=="*":
+                    while stack and (stack[-1] == "-" or stack[-1]=="/" or stack[-1]=="*"):
+                        queue.append(stack.pop())
+            #this check seves a similar purpose as above, only this time
+            #Div is in the shoes of Minus, and Mul in the shoes of Plus
+            elif c=="*" or c=="/":
+                if stack[-1] == "/":
+                    while stack and stack[-1]=="/":
+                        queue.append(stack.pop())
+            #either way, append the op to the stack, and advance i
+            stack.append(c)
+            i+=1
+    #DONE EXTRACTING TOKENS
+    #print("DONE EXTRACTING TOKENS.Emptying stack onto the queue...")
+    while stack:
+        queue.append(stack.pop())
+    #Now to perform the computations.
+    #Until the queue is empty, I must follow these steps:
+    # 1.empty queue by FIFO into the stack, until meeting an op
+    # 2. perform stack[-2] op stack[-1], replacing them with the result
+    # 3. continue scanning for numbers and ops until empty
+    while queue:
+        if isinstance(queue[0],Num):
+            stack.append(queue.pop(0))
+        else:#now we have a binary operator
+            B = stack.pop()
+            A = stack.pop()
+            op = queue.pop(0)
+            if op == "+":
+                stack.append(Plus(A,B))
+            elif op == "-":
+                stack.append(Minus(A,B))
+            elif op =="*":
+                stack.append(Mul(A,B))
+            else:
+                stack.append(Div(A,B))
+    res = stack.pop().calc()
+    #print(f"calc result: {res}")
+    return res
 
-    return S.pop().calc()
+def number_end(e,startindex):
+    encountered_dot = False
+    end = startindex+1
+    while end<len(e) and e[end].isnumeric() or (not encountered_dot and e[end]=="."):
+        if e[end]==".":
+            encountered_dot = True
+        end+=1
+    return end
 
-def extract_whole_number(expression,start_index):
-    i = start_index
-    while i<len(expression) and expression[i].isnumeric():#needed i<len(expression), otherwise error when the last
-                                                          #characters form a number
-        i+=1
-
-    return i
+#parser("(-3)+17*5-(12/4*2)")
